05316e4545f52f330bae3891c4cc8dca54c42d9c
Daniel Gorter <danieljgorter@gmail.com>
2020-06-04 14:54:07 -0400

    toggle view password

--- frontend/components/session/login/login_input.jsx
+++ frontend/components/session/login/login_input.jsx
@@ -5,10 +5,11 @@ class LoginInput extends React.Component {
   constructor(props){
     super(props);
 
-    this.state = { focus: false }
+    this.state = { focus: false, curType: this.props.type }
 
     this.handleFocus = this.handleFocus.bind(this)
     this.handleLeave = this.handleLeave.bind(this)
+    this.togglePasswordVisible = this.togglePasswordVisible.bind(this)
   }
 
   handleFocus(e){
@@ -19,14 +20,24 @@ class LoginInput extends React.Component {
     this.setState({ focus: false})
   }
 
+  togglePasswordVisible(){
+    const newType = (this.state.curType === 'text') ? 'password' : 'text'; 
+    this.setState({curType: newType})
+  }
+
   render(){
 
-    const { errorMsg, text, value, completed, ...rest } = this.props;
+    const { errorMsg, type, text, value, completed, ...rest } = this.props;
     const activated = Boolean(value || this.state.focus);
     const erroredOut = Boolean(errorMsg && !completed);
 
     const errorEle = erroredOut ? <div className="login-error-msg">{errorMsg}</div> : null
     const smallLabel =  activated ? <div className="small-input-label">{text}</div> : null
+    const passwordButton = (type === 'password') ? (
+      <span className='view-password-button' onClick={this.togglePasswordVisible}>
+        <i class="fas fa-eye"></i>
+      </span>
+    ) : null
 
     return (
   
@@ -36,8 +47,10 @@ class LoginInput extends React.Component {
             placeholder={ activated || completed ? null : text} 
             onFocus={this.handleFocus} 
             onBlur={this.handleLeave}
+            type={this.state.curType}
             {...rest}
           />
+          {passwordButton}
           {smallLabel}
         </div>
         {errorEle}
